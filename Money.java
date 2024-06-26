f7740547e3030118758af1b663bc790d04ca25fd
Pratiksha-M2907 <pratiksham.xworkz@gmail.com>
2024-06-26 22:17:30 +0530

    Wednesday, 26/06/2024 Task

---
+++ Money.java
@@ -0,0 +1,12 @@
+class Money {
+public static void main (String args[]){
+    int money = 87;
+        System.out.println(" I initially had " +money+" Rs");
+		System.out.println(" I found one Rupee coin in my purse");
+        ++money;
+        System.out.println(" Total " +money+" Coins i have right now ");
+		System.out.println(" I buyed a choclate of cost one rupee");
+		--money;
+		System.out.println(" I have "+money+" Rs in my purse Now");
+}
+}
---
+++ Posts.java
@@ -0,0 +1,12 @@
+class Posts {
+public static void main (String args[]){
+    int posts = 87;
+        System.out.println("Inititial My Insta Posts were: " +posts);
+		System.out.println("Recently I added one new post");
+        ++posts;
+        System.out.println("Number of posts after new adds are: " +posts);
+		System.out.println("Yestarday I deleted a post");
+		--posts;
+		System.out.println("Thus Total No. of posts in my profile are: " +posts);
+}
+}
