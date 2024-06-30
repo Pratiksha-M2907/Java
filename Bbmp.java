75b5ffd997c6dc52bf94d31463d4ec57f298a0e9
Pratiksha-M2907 <pratiksham.xworkz@gmail.com>
2024-06-30 22:37:34 +0530

    Friday Task 28/06/2024

---
+++ Bbmp.java
@@ -0,0 +1,11 @@
+public class Bbmp{
+ 
+ public static void bbmp(){
+
+ System.out.println(" BBMP: Bruhat Bengaluru Mahanagara Palike");
+ }
+ public static void bbmp_info(){
+
+ System.out.println(" Bruhat Bengaluru Mahanagara Palike (BBMP) is the administrative body \n responsible for civic amenities and some infrastructural assets of the Greater Bengaluru metropolitan area.\n It is the fourth largest Municipal Corporation in India \n and is responsible for a population of 8.4 million in an area of 741 km2\n");
+ }
+}
---
+++ BbmpRunner.java
@@ -0,0 +1,9 @@
+public class BbmpRunner{
+public static void main(String[] bbmp){
+System.out.println(" Loading Method\n");
+Bbmp.bbmp();
+System.out.println(" WHAT IS BBMP? \n");
+Bbmp.bbmp_info();
+}
+
+}
