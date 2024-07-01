1699c86c1842ee089c02a18c1218f0baf347910a
Pratiksha Maradimath <pratiksham.xworkz@gmail.com>
2024-07-02 01:22:55 +0530

    Delete BusStationRunner.java

--- BusStationRunner.java
+++
@@ -1,9 +0,0 @@
-class BusStationRunner{
-	public static void main(String args[]){
-		BusStation.getBusNumber("KA241236");
-		BusStation.getBusSchedule();
-		BusStation.getTicketPrice(7);
-		BusStation.TicketCancel();
-		BusStation.Bus_status();
-	}
-}
