b0868c6ef6be395b43f5e696ffe5149af8cfe962
Pratiksha-M2907 <pratiksham.xworkz@gmail.com>
2024-07-04 01:16:46 +0530

    Tuesday 3/07/2024 Task

---
+++ WaterrRunner.java
@@ -0,0 +1,10 @@
+public class WaterrRunner{
+
+public static void main(String args[]){
+
+Water. boilWater( 100);
+Water.addIceCubes( 5);
+Water.filterWater("Brita");
+Water.addFlavoring( "Lemon");
+}
+}
